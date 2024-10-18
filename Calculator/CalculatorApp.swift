4b5dc068b0b3029a68a25a3b313e3f7defcb2898
ZiXiang <wzxiang9301@gmail.com>
2024-10-19 01:17:58 +0800

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 翁子翔 on 2024/10/19.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by 翁子翔 on 2024/10/19.
+//
+
+import Testing
+@testable import Calculator
+
+struct CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
